a27cce07bba05c8de17f323d6c7e45bf2ee5ec09
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-06-05 11:33:09 -0300

    voltar login

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -4,5 +4,5 @@ import { createClient } from '@base44/sdk';
 // Create a client with authentication required
 export const base44 = createClient({
   appId: "681abf384445610e1ee1321d", 
-  requiresAuth: false // Ensure authentication is required for all operations
+  requiresAuth: true // Ensure authentication is required for all operations
 });
